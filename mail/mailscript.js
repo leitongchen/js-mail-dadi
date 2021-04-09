22e63692752cf03f32f512042d8959c3f758aedd
leitongchen <leitongchen93@gmail.com>
2021-04-09 16:34:12 +0200

    inserito un controllo all'es Mail

--- mail/mailscript.js
+++ mail/mailscript.js
@@ -1,23 +1,31 @@
 // utente inserisce la sua mail
-var userMail = prompt("Gentile membro della famiglia Simpson, inserisci qui la tua mail").toLowerCase;
-
+var userMail = prompt("Gentile membro della famiglia Simpson, inserisci qui la tua mail").toLowerCase();
+userMail 
 var accessPermitted = ["homer@mail.com", "marge@mail.com", "lisa@mail.com", "bart@mail.com", "maggie@mail.com"];
 
 // console.log("La mail inserita è " + userMail + ". L'accesso è permesso ai seguenti utenti " + accessPermitted);
 
-var mailExist = false;
 
-for (var i = 0 ; i < accessPermitted.length ; i++) {
+if (userMail === "" || userMail === null) {
+    console.log("Non hai inserito nulla.");
+
+} else {
+
+    var mailExist = false;
     
-    var singleMail = accessPermitted[i];
-
-    if (userMail === singleMail) {
-
-        mailExist = true;
-
-        console.log(userMail + " hai avuto accesso alla tua mail. Eri il nr " + (i + 1) + " in lista.")
+    for (var i = 0 ; i < accessPermitted.length ; i++) {
+        
+        var singleMail = accessPermitted[i];
+    
+        if (userMail === singleMail) {
+    
+            mailExist = true;
+    
+            console.log(userMail + " hai avuto accesso alla tua mail. Eri il nr " + (i + 1) + " in lista.")
+        }
     }
-}
-if (!mailExist) {
-    console.log("La mail che hai inserito non è presente nel database. Inserire una delle seguenti " + accessPermitted);
+    if (!mailExist) {
+        console.log("La mail che hai inserito non è presente nel database. Inserire una delle seguenti opzioni " + accessPermitted.join(", "));
+    }
+
 }
