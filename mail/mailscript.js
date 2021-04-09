9b26f82d439865a87b9013bf61806e111903ef9e
leitongchen <leitongchen93@gmail.com>
2021-04-09 15:36:52 +0200

    Esercizio mail

---
+++ mail/mailscript.js
@@ -0,0 +1,23 @@
+// utente inserisce la sua mail
+var userMail = prompt("Gentile membro della famiglia Simpson, inserisci qui la tua mail").toLowerCase;
+
+var accessPermitted = ["homer@mail.com", "marge@mail.com", "lisa@mail.com", "bart@mail.com", "maggie@mail.com"];
+
+// console.log("La mail inserita è " + userMail + ". L'accesso è permesso ai seguenti utenti " + accessPermitted);
+
+var mailExist = false;
+
+for (var i = 0 ; i < accessPermitted.length ; i++) {
+    
+    var singleMail = accessPermitted[i];
+
+    if (userMail === singleMail) {
+
+        mailExist = true;
+
+        console.log(userMail + " hai avuto accesso alla tua mail. Eri il nr " + (i + 1) + " in lista.")
+    }
+}
+if (!mailExist) {
+    console.log("La mail che hai inserito non è presente nel database. Inserire una delle seguenti " + accessPermitted);
+}
