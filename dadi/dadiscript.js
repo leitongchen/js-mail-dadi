baec65bce06600a94b6f8c5c8d229505609444be
leitongchen <leitongchen93@gmail.com>
2021-04-09 17:12:25 +0200

    correzione condizioni - es dadi

--- dadi/dadiscript.js
+++ dadi/dadiscript.js
@@ -5,7 +5,7 @@ var userNum = Math.ceil(Math.random() * 6);
 var aiNum = Math.ceil(Math.random() * 6);
 
 console.log("Questo è il numero random generato per te umano " + userNum);
-console.log("Questo è il numero random generato da e per il computer " + userNum);
+console.log("Questo è il numero random generato da e per il computer " + aiNum);
 
 
 //condizioni di vittoria 
@@ -13,11 +13,11 @@ console.log("Questo è il numero random generato da e per il computer " + userNu
 if (userNum > aiNum) {
     console.log("Umano hai vinto il gioco! Il tuo numero è " + userNum + " mentre il computer ha ottenuto un " + aiNum + ".");
 
-} else if (userNum = aiNum) {
+} else if (userNum < aiNum) {
+    console.log("Umano hai perso il gioco! Il tuo numero è " + userNum + " mentre il computer ha ottenuto un " + aiNum);
+
+} else  {
     console.log("Tu e il computer avete pareggiato.");
 
 }
- else {
-    console.log("Umano hai perso il gioco! Il tuo numero è " + userNum + " mentre il computer ha ottenuto un " + aiNum);
-
-}
+ 
