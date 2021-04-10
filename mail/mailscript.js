346b45874dd2cc330104914671e3fbf5124acef1
leitongchen <leitongchen93@gmail.com>
2021-04-10 11:24:10 +0200

    aggiunto controllo sul tipo di indirizzo mail

--- mail/mailscript.js
+++ mail/mailscript.js
@@ -1,13 +1,24 @@
 // utente inserisce la sua mail
-var userMail = prompt("Gentile membro della famiglia Simpson, inserisci qui la tua mail").toLowerCase();
-userMail 
+var userMail = prompt("Gentile membro della famiglia Simpson, inserisci qui la tua mail").toLowerCase().trim();
+
+
+
 var accessPermitted = ["homer@mail.com", "marge@mail.com", "lisa@mail.com", "bart@mail.com", "maggie@mail.com"];
 
 // console.log("La mail inserita è " + userMail + ". L'accesso è permesso ai seguenti utenti " + accessPermitted);
 
+var mailId = "@mail.com"
+
+userInputId = userMail.slice(-9);
 
 if (userMail === "" || userMail === null) {
-    console.log("Non hai inserito nulla.");
+    alert("Non hai inserito nulla.");
+
+} else if (Number(userMail)) {
+    alert("Non è valido inserire solo numeri.")
+
+} else if (userInputId !== mailId) {
+    alert("Non hai inserito un indirizzo mail valido. Sono permessi solo indirizzi mail di tipo " + mailId);
 
 } else {
 
